2d403664acf9f1ea85702708c420607cfd8869fb
mykhailo basiuk <wacok.mail.ru+github@gmail.com>
2020-05-26 23:06:30 -0500

    add https support and extentions

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-var wsUri = 'ws://207.191.157.79';
-var websocket = new WebSocket(wsUri);
+var mb = {};
+
+if (window.location.protocol === 'https') {
+    mb.uri = 'wss://207.191.157.79';
+} else {
+    mb.uri = 'ws://207.191.157.79';
+}
+
+mb.ws = new WebSocket(mb.uri);
+mb.onerror = function (ev) {
+    console.error(ev);
+};
+
 var performance = document.performance;
 
-websocket.addEventListener('error', function (ev) {
-    console.error(ev);
-});
-
-websocket.addEventListener('open', function (ev) {
+mb.onopen = function (ev) {
     var collected = JSON.stringify(performance);
     console.log('open', ev, performance, collected);
     websocket.send(collected);
-});
+};
 
-websocket.addEventListener('close', function (ev) {
+mb.onclose = function (ev) {
     console.log('close', ev);
-});
+};
 
-websocket.addEventListener('message', function (ev) {
+mb.onmessage = function (ev) {
     console.log('message', ev)
-});
+};
+
+if(window.mbx) {
+    mb = Object.assign(mb, mbx);
+}
+
+mb.ws.addEventListener('error', mb.onerror);
+
+mb.ws.addEventListener('open', mb.onopen);
+
+mb.ws.addEventListener('close', mb.onclose);
+
+mb.ws.addEventListener('message', mb.onmessage);
